aa572aa2083f54e54eb9c5ab2f5c7f892a779318
RicardoFilipe99 <57844312+RicardoFilipe99@users.noreply.github.com>
2021-05-18 19:18:17 +0100

    core-data code simplified, namely, code referente to adding mock data will be on a separate module

--- Project/core-data.js
+++ Project/core-data.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const error = require('./i-on-web-errors.js');
-const mockData = require('./mock-data.js')();
 const fetch = require('node-fetch'); 
 
 const contentType = 'application/json';
@@ -10,12 +9,6 @@ const contentType = 'application/json';
 const read_authorization = 'Bearer ' + process.env.CORE_READ_TOKEN;
 const core_url = process.env.CORE_URL;
 
-// Test simulating a user (to delete)
-const user = {
-	username: "user",
-	password: "123",
-	selectedCoursesAndClasses: {} /// {"1": ["1D", "1N", ..], "2": []}
-};
 
 const coreRequest = async function(uri, method, expectedStatus, reqBody) {
 
@@ -38,24 +31,7 @@ module.exports = function() {
 
 	const loadAllProgrammes = async function () {
 		try {
-			const receivedData = await coreRequest(core_url + '/v0/programmes/', 'GET', 200);
-	
-			/* Adding missing data */
-			const responseMockData = await mockData.loadAllProgrammes();
-			const responseWithAddedData = receivedData.entities
-			.map( entities => {
-				const programme = responseMockData.entities
-				.filter( mockEntities => mockEntities.properties.programmeId == entities.properties.programmeId)[0].properties
-				
-				entities.properties["name"] = programme.name;
-				entities.properties["degree"] = programme.degree;
-
-				return entities;
-			});
-			const response = {'entities': responseWithAddedData};
-
-			return response;
-
+			return await coreRequest(core_url + '/v0/programmes/', 'GET', 200);
 		} catch(err) {
 			switch (err) {
 				case 404: /// Not Found
@@ -68,29 +44,7 @@ module.exports = function() {
 
 	const loadAllProgrammeOffers = async function (programmeId) {
 		try {
-			const receivedData = await coreRequest(core_url + '/v0/programmes/'+ programmeId, 'GET', 200);
-
-			/* Adding missing data */ 
-			const responseMockData = await mockData.loadAllProgrammeOffers(programmeId);
-
-			const responseWithAddedData = receivedData.entities
-			.map( entities => {
-				const offer = responseMockData.entities
-				.filter( mockEntities => mockEntities.properties.courseId == entities.properties.courseId)[0].properties;
-
-				entities.properties["name"] = offer.name;
-				entities.properties["acronym"] = offer.acronym;
-				entities.properties["optional"] = offer.optional;
-				entities.properties["ects"] = offer.ects;
-				entities.properties["scientificArea"] = offer.scientificArea;
-				return entities;
-			});
-
-			const response = {'entities': responseWithAddedData};
-
-			return response;
-
-		
+			return await coreRequest(core_url + '/v0/programmes/'+ programmeId, 'GET', 200);	
 		} catch(err) {
 			switch (err) {
 				case 404: /// Not Found
@@ -101,23 +55,9 @@ module.exports = function() {
 		}
 	};
 
-	const loadProgrammeData = async function (programmeId) {
+	const loadProgrammeData = async function(programmeId) {
 		try {
-			const receivedData = await coreRequest(core_url + '/v0/programmes/'+ programmeId, 'GET', 200);
-		
-			/* Adding missing data */
-			const responseMockData = await mockData.loadProgrammeData(programmeId);
-
-			receivedData.properties.name = responseMockData.properties.name;
-			receivedData.properties["department"] = responseMockData.properties.department;
-			receivedData.properties["department"] = responseMockData.properties.department;
-			receivedData.properties["coordination"] = responseMockData.properties.coordination;
-			receivedData.properties["contacts"] = responseMockData.properties.contacts;
-			receivedData.properties["sourceLink"] = responseMockData.properties.sourceLink;
-			receivedData.properties["description"] = responseMockData.properties.description;
-
-			return receivedData;
-
+			return await coreRequest(core_url + '/v0/programmes/'+ programmeId, 'GET', 200);
 		} catch(err) {
 			switch (err) {
 				case 404: /// Not Found
@@ -128,24 +68,9 @@ module.exports = function() {
 		}
 	};
 
-	const loadCourseByID = async function(courseId) {
+	const loadCourse = async function(courseId) {
 		try {
-			const receivedData = await coreRequest(core_url + '/v0/courses/'+ courseId +'/classes/1718i', 'GET', 200); // TO DO - change 
-
-			/* Adding missing data */
-			const responseMockData = await mockData.loadCourseByID(courseId);
-
-			const responseWithAddedData = receivedData.entities
-			.filter(entities => entities.properties.hasOwnProperty('id'))
-			.map( entities => {
-				entities.properties["name"] = responseMockData.entities[0].properties.name; 
-				return entities;
-			});
-
-			const response = {'entities': responseWithAddedData};
-
-			return response;
-
+			return await coreRequest(core_url + '/v0/courses/'+ courseId +'/classes/1718i', 'GET', 200); // TO DO - change 
 		} catch (err) {
 			switch (err) {
 				case 404: /// Not Found
@@ -155,10 +80,10 @@ module.exports = function() {
 			}
 		}
 	}
+	
 	const loadAboutData = async function () {
 		try {
-			/* Adding missing data */ 
-			return await mockData.loadAboutData();
+			return {}; // Request still not suported by i-on Core
 		} catch(err) {
 			switch (err) {
 				case 404: /// Not Found
@@ -169,46 +94,11 @@ module.exports = function() {
 		}
 	};
 
-	
-	// Test functions (to delete)
-	const loadUser = async function() {
-		try {
-			return user;
-		} catch (err) {
-			// TO DO - Handle errors
-		}
-	};
-
-	const saveUserCoursesAndClasses = async function(body) {
-		try { 
-			// avoid: substitution and repetition
-			for (const prop in body) { /// Iterate over body properties (choosen courses)
-				if (body.hasOwnProperty(prop) ) { 
-
-					if(!user.selectedCoursesAndClasses.hasOwnProperty(prop)) { /// If the user has not yet chosen that course 
-						user.selectedCoursesAndClasses[prop] = body[prop];
-					} else { /// If the user has already chosen classes from that course, then it will be added to the array (we filter classes first to avoid repetitions)
-						const newClasses = body[prop]
-						.filter(
-							courseClass => !user.selectedCoursesAndClasses[prop].includes(courseClass)
-						);
-						user.selectedCoursesAndClasses[prop] = user.selectedCoursesAndClasses[prop].concat(newClasses);
-					} 
-			
-				}
-			}
-		} catch (err) {
-			// TO DO - Handle errors
-		}
-	};
-
 	return {
         loadAllProgrammes : loadAllProgrammes,
 		loadAllProgrammeOffers : loadAllProgrammeOffers,
 		loadProgrammeData : loadProgrammeData,
-		loadCourseByID : loadCourseByID,
-		loadAboutData : loadAboutData,
-		saveUserCoursesAndClasses : saveUserCoursesAndClasses,
-		loadUser : loadUser
+		loadCourse : loadCourse,
+		loadAboutData : loadAboutData
 	};
 }
