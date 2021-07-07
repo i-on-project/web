8e2f560e04b2c68e63c4278d0b75b8dc47bc7542
Catarina Palma <catarina.1999.palma@gmail.com>
2021-07-07 12:03:09 +0100

    Creating basic versions of integration tests and adding some mock data

--- project/presentation-layer/i-on-web-ui.js
+++ project/presentation-layer/i-on-web-ui.js
@@ -168,28 +168,28 @@ function webui(service, auth) {
 
 	/******* Mapping requests to handlers according to the path *******/
 
-	router.get(	'/', 					theWebUI.home							);	/// Home page
+	router.get(	'/', 					theWebUI.home											);	/// Home page
 
-	router.get(	'/programme/:id', 		theWebUI.programme						);	/// Programme info page
-	router.get(	'/programme-offers/:id',theWebUI.programmeCalendarTermOffers	); 	/// Programme offers page
+	router.get(	'/programme/:id', 		theWebUI.programme										);	/// Programme info page
+	router.get(	'/programme-offers/:id',theWebUI.programmeCalendarTermOffers					); 	/// Programme offers page
 	
 	router.get(	'/available-class-sections',	theWebUI.classSectionsFromSelectedClasses		);		/// Available classes of the selected courses
-	router.post('/class-sections', 			theWebUI.saveUserClassesAndClassSections);	/// todo review
+	router.post('/class-sections', 			theWebUI.saveUserClassesAndClassSections			);	/// todo review
 
 	router.get(	'/classes',				theWebUI.userClassesAndClassSections					); 	/// Users courses page
 	router.get(	'/classes/edit',		theWebUI.userClassesAndClassSectionsEditUI				);
 	router.post('/classes/edit',		theWebUI.userClassesAndClassSectionsEdit				);
-	router.get(	'/schedule', 			theWebUI.userSchedule					);	/// Users schedule page
-	router.get(	'/calendar', 			theWebUI.userCalendar					);	/// Users calendar page
+	router.get(	'/schedule', 			theWebUI.userSchedule									);	/// Users schedule page
+	router.get(	'/calendar', 			theWebUI.userCalendar									);	/// Users calendar page
 
-	router.get(	'/about', 				theWebUI.about							);	/// About Page
-	router.get( '/profile', 			theWebUI.profile						);  /// User Profile Page
-	router.post('/profile', 			theWebUI.editProfile					);
-	router.get(	'/delete-user',         theWebUI.deleteUser						);
+	router.get(	'/about', 				theWebUI.about											);	/// About Page
+	router.get( '/profile', 			theWebUI.profile										);  /// User Profile Page
+	router.post('/profile', 			theWebUI.editProfile									);
+	router.get(	'/delete-user',         theWebUI.deleteUser										);
 
 	/*** Auth ***/
-	router.get(	'/login',				theWebUI.loginUI			   			);	/// Login UI page
-	router.get(	'/logout',				theWebUI.logout							);
+	router.get(	'/login',				theWebUI.loginUI			   							);	/// Login UI page
+	router.get(	'/logout',				theWebUI.logout											);
 
 	return router;
 }
