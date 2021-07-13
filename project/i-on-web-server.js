b393ed719b2f34ddf50835ae1f92a1369f7b1d40
RicardoFilipe99 <57844312+RicardoFilipe99@users.noreply.github.com>
2021-07-13 14:49:35 +0100

    Update i-on-web-server.js

--- project/i-on-web-server.js
+++ project/i-on-web-server.js
@@ -31,11 +31,11 @@ async function configurations() {
     /// Data
     let data;
 
-   // if(process.env.OPERATION_MODE === "standalone") {
+    if(process.env.OPERATION_MODE === "standalone") {
 
         data = require(`${dataAccessLayerPath}/mock-data.js`)();
 
-   /* } else {
+    } else {
 
         const core = require(`${dataAccessLayerPath}/core-data.js`)();
 
@@ -46,7 +46,7 @@ async function configurations() {
         const metadata = require(`${businessLogicLayerPath}/remove-metadata.js`)(cache);
         
         data = metadata;
-    }*/
+    }
 
     /// Auth
     const auth = require(`${businessLogicLayerPath}/i-on-web-auth.js`)(app, data, sessionDB);
@@ -81,7 +81,7 @@ async function configurations() {
         res.status(404);
         res.render('errorPage', { status: 404, errorMessage: 'Not Found' });
     });
-
+    
     app.set('view engine', 'hbs') /// Setting the template engine to use (hbs)
 
     app.listen(port);
